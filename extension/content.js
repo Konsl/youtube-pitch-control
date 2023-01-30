2770e5c2efc05682abaf3e427c4e71460352e49f
Konsl <82901383+Konsl@users.noreply.github.com>
2023-01-30 20:34:19 +0100

    Support older versions of Firefox

--- extension/content.js
+++ extension/content.js
@@ -4,13 +4,21 @@ function updateVideoAttributes(preservesPitch) {
     const videos = document.getElementsByTagName("video");
     console.log(`[Youtube Pitch Control] Found ${videos.length} video elements...${videos.length == 0 ? 'Retrying in 200 ms...' : ''}`);
 
-    if(videos.length == 0){
+    if (videos.length == 0) {
         setTimeout(updateVideoAttributes.bind(this, preservesPitch), 200);
         return;
     }
 
-    for (const video of document.getElementsByTagName("video"))
-        video.preservesPitch = preservesPitch;
+    for (const video of document.getElementsByTagName("video")) {
+        if ("preservesPitch" in video)
+            video.preservesPitch = preservesPitch;
+        else if ("mozPreservesPitch" in video)
+            video.mozPreservesPitch = preservesPitch;
+        else {
+            console.log("[Youtube Pitch Control] Error: preservesPitch is not supported (should have been detected by min version attribute in manifest)");
+            return;
+        }
+    }
 }
 
 storageAPI.sync.get(["disablePitchShifting"], (res) => {
